334467cbab7c9e4fcc2281aac5cc01e8aae5e3b8
sowmya-debug <sowmya-debug@example.com>
2026-08-21 10:16:47 +0530

    Restore missing express, mongoose, cors, helmet, rateLimit imports in server/index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,8 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const path = require('path');
 require('dotenv').config();
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
